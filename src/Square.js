5bc2e045a8af483d9aea7f2bed04bedf28d1b243
rominalodolo <lodoloromina@gmail.com>
2021-11-25 09:28:25 +0200

    board and square

---
+++ src/Square.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Square = () => {
+  return (
+    <div>
+      <button className="square">{this.props.value}</button>
+    </div>
+  );
+};
+
+export default Square;
